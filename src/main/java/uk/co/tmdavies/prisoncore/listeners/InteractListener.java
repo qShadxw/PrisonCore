0e6e3528a8e1acceb643b7ffacf768b88b89652c
Ben <demonfly3215@gmail.com>
2023-02-23 21:12:59 -0800

    Added an inventory to show how many blocks the player has absorbed.

--- src/main/java/uk/co/tmdavies/prisoncore/listeners/InteractListener.java
+++ src/main/java/uk/co/tmdavies/prisoncore/listeners/InteractListener.java
@@ -2,16 +2,20 @@ package uk.co.tmdavies.prisoncore.listeners;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import uk.co.tmdavies.prisoncore.PrisonCore;
 import uk.co.tmdavies.prisoncore.objects.Profile;
+import uk.co.tmdavies.prisoncore.utils.Utils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
 
 public class InteractListener implements Listener {
 
@@ -20,6 +24,7 @@ public class InteractListener implements Listener {
     public static HashMap<Material, Double> blockValues = new HashMap<Material, Double>();
     public static String worldName;
     private final String baItem;
+    public static Inventory absorbInventory;
 
     public InteractListener(PrisonCore plugin) {
 
@@ -37,6 +42,8 @@ public class InteractListener implements Listener {
 
     @EventHandler
     public void onBlockClickEvent(PlayerInteractEvent event) {
+        Action action = event.getAction();
+        if (!action.equals(Action.LEFT_CLICK_BLOCK)) { return; }
         if (!event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.getMaterial(Objects.requireNonNull(baItem)))) { return; }
         if (event.getClickedBlock() == null) { return; }
         if (event.getClickedBlock().getType() == Material.AIR) { return; }
@@ -47,6 +54,32 @@ public class InteractListener implements Listener {
         profile.addAbsorbedBlock(blocktype, 1);
         event.getClickedBlock().setType(Material.AIR);
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onRightClickEvent(PlayerInteractEvent event) {
+        Action action = event.getAction();
+        if (!action.equals(Action.RIGHT_CLICK_AIR) && !action.equals(Action.RIGHT_CLICK_BLOCK)) { return; }
+        if (!event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.getMaterial(Objects.requireNonNull(baItem)))) { return; }
+        ItemStack i = event.getPlayer().getInventory().getItemInMainHand();
+        if (!i.equals(JoinListener.baItem)) { return; }
+        absorbInventory = Bukkit.createInventory(null, 27, Utils.Colour(Objects.requireNonNull(JoinListener.baItem.getItemMeta()).getDisplayName()));
+        Profile pro = PrisonCore.playerProfiles.get(event.getPlayer());
+        for (Material material : pro.getAbsorbedBlocks().keySet()) {
+            ItemStack item = new ItemStack(material);
+            ItemMeta itemM = item.getItemMeta();
+            assert itemM != null;
+            itemM.setDisplayName(Utils.Colour("&7&l" + item.getType().toString().toUpperCase()));
+            itemM.setLore(Collections.singletonList(Utils.Colour("&a&lx" + pro.getAbsorbedBlocks().get(material))));
+            item.setItemMeta(itemM);
+            absorbInventory.addItem(item);
+        }
+        event.getPlayer().openInventory(absorbInventory);
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        // Add the ability to stop players taking items out of inventories.
     }
 
 }
